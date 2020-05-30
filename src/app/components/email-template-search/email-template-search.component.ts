f4500db69cebb849a9f334bbd14b4ecad2a67779
Mark Toscano <meathookjones@gmail.com>
2020-05-30 10:59:07 -0400

    Moved logic for converting blurb raw text to objects to the service.

--- src/app/components/email-template-search/email-template-search.component.ts
+++ src/app/components/email-template-search/email-template-search.component.ts
@@ -15,26 +15,9 @@ export class EmailTemplateSearchComponent implements OnInit {
   constructor( private emailItemsService: EmailItemsService, private emailTemplatesService: EmailTemplatesService) { }
 
   ngOnInit() {
-    this.emailTemplatesService.getEmailTemplates().subscribe((rawText) => {
-      const rawTemplates = rawText.trim().split('***');
-
-      rawTemplates.forEach((entry) => {
-        let cleanTemplate = entry.replace(/[\n\r]/g, ' ');
-        const templateFields = cleanTemplate.split('### ');
-
-        if(templateFields && templateFields[0] && templateFields[1]) {
-          this.templates.push({
-            name: templateFields[0].trim(),
-            blurb: templateFields[1].trim()
-          })
-        } else {
-          console.log('ERROR parsing template:');
-          console.info(templateFields)
-        }
-      })
-
-      console.info(this.templates);
-
+    this.emailTemplatesService.getEmailTemplates().subscribe((emailTemplates) => {
+      console.info(emailTemplates);
+      this.templates = this.emailTemplatesService.convertRawText(emailTemplates);
       this.emailItemsService.setEmailItems(this.templates);
     })
   }
--- src/app/services/email-templates.service.ts
+++ src/app/services/email-templates.service.ts
@@ -10,6 +10,29 @@ export class EmailTemplatesService {
   constructor(private httpClient: HttpClient) { }
 
   getEmailTemplates() : Observable<any> {
-    return this.httpClient.get('assets/files/blurbs.txt', { responseType: 'text' });
+    return this.httpClient.get('assets/files/blurbs.txt', {responseType: 'text'});
+  }
+
+  //TEMP
+  public convertRawText(rawText): any {
+    const rawTemplates = rawText.trim().split('***');
+    const emailTemplates = [];
+
+    rawTemplates.forEach((entry) => {
+      let cleanTemplate = entry.replace(/[\n\r]/g, ' ');
+      const templateFields = cleanTemplate.split('### ');
+
+      if (templateFields && templateFields[0] && templateFields[1]) {
+        emailTemplates.push({
+          name: templateFields[0].trim(),
+          blurb: templateFields[1].trim()
+        })
+      } else {
+        console.log('ERROR parsing template:');
+        console.info(templateFields)
+      }
+    })
+
+    return emailTemplates;
   }
 }
